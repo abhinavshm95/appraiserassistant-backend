c7a99d22dfd32e82e7707c81d7f2d4c5f4978cea
abhinavshm95 <abhinavshm95@gmail.com>
2025-12-20 16:39:57 +0530

    feat: add 'manager' to user roles enum in schema

--- model/User.js
+++ model/User.js
@@ -1,57 +1,60 @@
-const Mongoose=require("mongoose")
+const Mongoose = require("mongoose");
 
-const userSchema=new Mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
-    phone:String,
-    otp:String,
-    accessToken:String,
-    isEmailVerified:{
-        type:Boolean,
-        default:false
+const userSchema = new Mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    phone: String,
+    otp: String,
+    accessToken: String,
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
     },
-    isPhoneVerified:{
-        type:Boolean,
-        default:false
+    isPhoneVerified: {
+      type: Boolean,
+      default: false,
     },
-    role:{
-        type:String,
-        enum:["standard","admin"],
-        default:"standard"
+    role: {
+      type: String,
+      enum: ["standard", "admin", "manager"],
+      default: "standard",
     },
     // OAuth fields
     googleId: {
-        type: String,
-        sparse: true,
-        index: true
+      type: String,
+      sparse: true,
+      index: true,
     },
     authProvider: {
-        type: String,
-        enum: ["local", "google"],
-        default: "local"
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
     },
     profilePicture: {
-        type: String
+      type: String,
     },
     // Single-device login: increments on each login to invalidate old sessions
     sessionVersion: {
-        type: Number,
-        default: 1
+      type: Number,
+      default: 1,
     },
     // Current active device info
     activeDevice: {
-        ip: String,
-        userAgent: String,
-        browser: String,
-        os: String,
-        device: String,
-        loginAt: Date
-    }
-}, {
-    timestamps: true
-})
+      ip: String,
+      userAgent: String,
+      browser: String,
+      os: String,
+      device: String,
+      loginAt: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const user=Mongoose.model("User",userSchema)
+const user = Mongoose.model("User", userSchema);
 
-module.exports=user
+module.exports = user;
